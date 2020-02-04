c5083c91c522cc390c75b68d0be07d52f650d273
Itok Toni Laksono <itok.tonilaksono@mitrais.com>
2020-02-04 10:15:04 +0700

    update

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -20,7 +20,7 @@ class ResetPasswordController extends Controller
 
     use ResetsPasswords;
 
-    protected $redirectTo = '/dashboard';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
